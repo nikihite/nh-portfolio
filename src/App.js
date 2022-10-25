751c519eb15a5de773853b7b38c1bbff4aeee481
Niki Hite <nikihite@comcast.net>
2022-10-25 14:39:22 -0700

    added nav links to each page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import logo from './logo.svg';
+/* eslint-disable indent */
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+import Projects from './Projects';
+import HomePage from './HomePage';
+import AboutMe from './AboutMe';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <Link className="nav-link" to="/">Home</Link>
+          <Link className="nav-link" to="/projects">Projects</Link>
+          <Link className="nav-link" to="/aboutme">About Me</Link>
+        </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              <Route exact path="/projects">
+                <Projects />
+              </Route>
+              <Route exact path="/aboutme">
+                <AboutMe />
+              </Route>
+            </Switch>
+      </div>
+    </Router>
   );
 }
 
