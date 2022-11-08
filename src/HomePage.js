60ef3a106f50667529320659f94aae4021d41f4e
Niki Hite <nikihite@comcast.net>
2022-11-08 11:23:37 -0800

    added name on home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,11 @@
+import './HomePage.css';
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div className='HomePage'>
+      <h1 className='hello-friends'>hello friends,</h1>
+      <h1 className='niki-logo1'>NIKI</h1>
+      <h1 className='niki-logo2'>HITE</h1>
+    </div>
   );
 }
