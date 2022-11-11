cc4a71b172478b35ff8dc4d73f24f1b3f2b6f620
Niki Hite <nikihite@comcast.net>
2022-11-11 15:19:19 -0800

    added images and hovor to projects

--- src/Projects.js
+++ src/Projects.js
@@ -13,10 +13,11 @@ export default function Projects() {
         <h1>Allegori</h1>
         <h2>React, Node, PostgreSQL</h2>
         <p>An Artificial Intelligence app allowing users to input an image and receive a meme based on AI analysis.</p>
+        <img className="proj-pic" src='../allegori-ss.png' />
       </div>
       <div className='pixie'>
         <h1>Pixie.js</h1>
-        <h2>JS, Node, Express, PostgreSQL, Heroku</h2>
+        <h2>JavaScript, Node, Express, PostgreSQL, Heroku</h2>
         <p>A tarot-based API celebrating the artwork of Pamela Colman Smith, allowing users to pick, track and update their pulls from the symbolic Rider-Waite Smith deck.</p>
         <img className="proj-pic" src="../99321BCC-8ABF-43D7-9CA2-5EB2F3A9CA0E_1_102_o.jpeg" />
       </div>
@@ -24,6 +25,7 @@ export default function Projects() {
         <h1>HYNT</h1>
         <h2>JavaScript, Node, Netlify, Supabase</h2>
         <p>A life or death text adventure game where you decide your own fate.</p>
+        <img className="proj-pic" src='../hynt-screenshot.png' />
       </div>
     </div>
   );
