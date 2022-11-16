d057263de1ce75f473c6bcde0ba0bbcd524b5db8
Niki Hite <nikihite@comcast.net>
2022-11-15 17:02:30 -0800

    added icons for contact me page

--- src/Contact.js
+++ src/Contact.js
@@ -4,18 +4,18 @@ export default function Contact() {
   return (
     <div>
       <h1 className='connect'>Connect with me!</h1>
-      <ul>
+      <ul className='contact-list'>
         <li>
-          <a href='https://www.linkedin.com/in/nikihite/'>LinkedIn</a>
+          <a className='contact-link' href='https://www.linkedin.com/in/nikihite/'>LinkedIn <img className='contactme-img' src='../linkedin.png' /></a>
         </li>
         <li>
-          <a href='https://github.com/nikihite'>GitHub</a>
+          <a className='contact-link' href='https://github.com/nikihite'>GitHub <img className='contactme-img' src='../github.png' /></a>
         </li>
         <li>
-          <a href='https://twitter.com/NikiMHite'>Twitter</a>
+          <a className='contact-link' href='https://twitter.com/NikiMHite'>Twitter <img className='contactme-img' src='../twitter.png'/></a>
         </li>
         <li>
-          <a href='mailto: nikihite3@gmail.com'>Email Me!</a>
+          <a className='contact-link' href='mailto: nikihite3@gmail.com'>Email Me! <img className='contactme-img' src='../email.png'/></a>
         </li>
       </ul>
     </div>
