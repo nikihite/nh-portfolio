e978522b7538e6708faa7fcfe36a778e24824436
Niki Hite <nikihite@comcast.net>
2022-12-06 13:00:17 -0800

    changed from React app to niki hite

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Projects from './Projects';
 import HomePage from './HomePage';
 import AboutMe from './AboutMe';
 import Contact from './Contact';
+import Resume from './Resume';
 import './App.css';
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
           <Link className="nav-link" to="/projects">Projects</Link>
           <Link className="nav-link" to="/about">About</Link>
           <Link className="nav-link" to="/contact">Contact</Link>
+          <Link className='nav-link' to="/resume">Resume</Link>
         </nav>
 
                 {/* A <Switch> looks through its children <Route>s and
@@ -36,6 +38,9 @@ function App() {
               </Route>
               <Route exact path="/contact">
                 <Contact />
+              </Route>
+              <Route exact path="/resume">
+                <Resume />
               </Route>
             </Switch>
       </div>
